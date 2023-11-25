ce6088a96147da749bd7410f23797596528cc9af
Vijay Koushik, S <svijaykoushik@hotmail.com>
2023-11-25 18:38:40 +0530

    chore: Updated maker config for deb files

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,5 @@
+const { join } = require("path");
+
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -5,29 +7,43 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
         authors: "Vijaykoushik, S.",
         name: "ChronoChime",
-        description: "Hourly Notification Desktop Application  with background sounds and notifications"
+        description:
+          "Hourly Notification Desktop Application  with background sounds and notifications",
+        iconUrl:
+          "https://raw.githubusercontent.com/svijaykoushik/chrono-chime-desktop/main/chrono-chime-icon.ico",
+        setupIcon: join(__dirname, "chrono-chime_package.ico"),
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          icon: join(__dirname, "chrono-chime-icon-512.png"),
+          maintainer: "Vijaykoushik, S.",
+          productName: "ChoronoChime",
+          homepage: "https://chrono-chime.web.app",
+          genericName: "Hourly Notification",
+          description:
+            "Hourly Notification Desktop Application  with background sounds and notifications",
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
