ea07167b79b40367254d3b6558e1c3a0b5b63aa6
Vijay Koushik, S <svijaykoushik@hotmail.com>
2024-06-16 19:59:01 +0530

    wip: Implement task list functionality  - Started working on task item rendering.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import {
   createList,
   addTask,
   getLists,
-  // getTasks
+  getTasks
 } from './renderer/to-do-lists/to-do-lists.js';
 
 import {
@@ -309,45 +309,60 @@ const tasksForm = document.getElementById('tasksForm');
 const taskListContainer = document.getElementById('taskListContainer');
 
 newListBtn.addEventListener('click', () => listsModal.showModal());
-newReminderBtn.addEventListener('click', () => remindersModal.showModal());
 cancelAddListBtn.addEventListener('click', () => listsModal.close());
 cancelAddTaskBtn.addEventListener('click', () => tasksModal.close());
+
+// eslint-disable-next-line no-unused-vars
+function openAddTaskModal(listId){
+  tasksModal.showModal();
+}
 
 // Example task list rendering
 async function renderTaskLists() {
   const lists = await getLists();
   taskListContainer.innerHTML = '';
-  let taskListItems = '';
+  const taskList = document.createElement('ul');
+  taskList.className='list';
+  // let taskListItems = '';
   for (const list of lists) {
-    taskListItems += `<li data-list-id="${list.id}">
-            <div class="flex-grow">
-              <span class="heading subtitle1">${list.name}</h2>
-            </div>
-            <div class="list-secondary-action">
-              <button type="button">
-                <span class="emoji">❌</span>
-              </button>
-            </div>
-            <div class="clear-float"></div>
-          </li>`;
-    // const tasks = await getTasks(list.id);
-    // const listElement = document.createElement('div');
-    // listElement.className = 'list';
-    // listElement.innerHTML = `
-    //       <h2>${list.name}</h2>
-    //       <button class="cta-button" onclick="openAddTaskModal(${list.id})"><span class="emoji">➕</span>Add Task</button>
-    //       <ul class="list">
-    //           ${tasks.map((task) => `
-    //               <li>
-    //                   <span>${task.task.title}</span>
-    //                   <span>${task.task.completed ? '✅' : '❌'}</span>
-    //                   <button onclick="deleteTask(${task.id})">Delete</button>
-    //               </li>`).join('')}
-    //       </ul>
-    //   `;
-    // taskListContainer.appendChild(listElement);
+    // taskListItems += `<li data-list-id="${list.id}">
+    //         <div class="flex-grow">
+    //           <span class="heading subtitle1">${list.name}</h2>
+    //         </div>
+    //         <div class="list-secondary-action">
+    //           <button type="button">
+    //             <span class="emoji">❌</span>
+    //           </button>
+    //         </div>
+    //         <div class="clear-float"></div>
+    //       </li>`;
+    const tasks = await getTasks(list.id);
+    const listElement = document.createElement('li');
+    listElement.setAttribute('data-list-id',list.id);
+    listElement.innerHTML = `<li data-list-id="${list.id}">
+         <div class="flex-grow">
+           <span class="heading subtitle1">${list.name}</h2>
+           <button class="cta-button" onclick="openAddTaskModal(${list.id})"><span class="emoji">➕</span>Add Task</button>
+          <ul class="list">
+              ${tasks.map((task) => `
+                  <li>
+                      <span>${task.task.title}</span>
+                      <span>${task.task.completed ? '✅' : '❌'}</span>
+                      <button onclick="deleteTask(${task.id})">Delete</button>
+                  </li>`).join('')}
+          </ul>
+         </div>
+         <div class="list-secondary-action">
+           <button type="button">
+             <span class="emoji">❌</span>
+           </button>
+         </div>
+         <div class="clear-float"></div>
+       </li>`;
+       taskList.appendChild(listElement);
   }
-  taskListContainer.innerHTML = `<ul class="list">${taskListItems}</ul>`;
+  // taskListContainer.innerHTML = `<ul class="list">${taskListItems}</ul>`;
+  taskListContainer.appendChild(taskList);
 }
 
 
@@ -357,6 +372,7 @@ addListBtn.addEventListener('click', async (e) => {
   if (listName.length >= 3 && listName.length <= 250) {
     await createList(listName);
     renderTaskLists();
+    listsModal.close();
   }
 });
 
@@ -368,6 +384,7 @@ addTaskBtn.addEventListener('click', async (e) => {
   if (taskDescription.length >= 3 && taskDescription.length <= 250) {
     await addTask(listId, { title: taskDescription, completed });
     renderTaskLists();
+    tasksModal.close();
   }
 });
 //#endregion
